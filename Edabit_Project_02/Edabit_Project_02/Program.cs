6d5a10c333198a24645c3dadfe68144ec9708c0b
CARICOM12 <0190a@ukr.net>
2020-05-03 15:10:14 +0300

    Add files via upload

---
+++ Edabit_Project_02/Edabit_Project_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        int FindDigitAmount(int num)
+        {
+            return num.ToString().Length;
+        }
+    }
+
+}
+
+/*Create a function that takes a number as an argument 
+ and returns the amount of digits it has.*/
